f24cd2e01611b8deff1f8ad26bd12d8271c4c5b6
unknown <manikanta.official1408@gmail.com>
2023-09-22 15:20:18 +0530

    initial commit

---
+++ BankLogin/Controllers/UserlogController.cs
@@ -0,0 +1,34 @@
+﻿using BankLogin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankLogin.Controllers
+{
+    public class UserlogController : Controller
+    {
+        private readonly Userlog user = new Userlog()
+        {
+            UserId = 100,
+            Password = "Rahul@123"
+        };
+        
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Userlog logs)
+        {
+            if(ModelState.IsValid && logs.UserId == user.UserId && logs.Password == user.Password)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+                return View(logs);
+            }
+            
+        }
+    }
+}
